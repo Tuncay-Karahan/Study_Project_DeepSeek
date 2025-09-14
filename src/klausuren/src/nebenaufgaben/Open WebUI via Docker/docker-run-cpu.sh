a886f8d8092cdfe05d87ca30592dcb15bf9aea0f
Tuncay Karahan <tuncay.karahan61@hotmail.com>
2025-09-14 20:53:46 +0200

    Delete src/klausuren directory

--- src/klausuren/src/nebenaufgaben/Open WebUI via Docker/docker-run-cpu.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-docker run -d \
-  -p 3000:8080 \
-  -e OLLAMA_BASE_URL=http://host.docker.internal:11434 \
-  -v open-webui:/app/backend/data \
-  --name open-webui \
-  --restart always \
-  ghcr.io/open-webui/open-webui:main
