43605b19dceedeb08e9225219a77d9d62a22bf2d
Tuncay Karahan <tuncay.karahan61@hotmail.com>
2025-09-14 21:02:32 +0200

    Delete scripts directory

--- scripts/run_all.ps1
+++
@@ -1,3 +0,0 @@
-python "src/klausuren/1_latex_to_json.py"
-python "src/klausuren/2_send_to_models.py"
-python "src/klausuren/3_compare_and_score.py"
--- scripts/run_all.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-python "src/klausuren/1_latex_to_json.py"
-python "src/klausuren/2_send_to_models.py"
-python "src/klausuren/3_compare_and_score.py"
